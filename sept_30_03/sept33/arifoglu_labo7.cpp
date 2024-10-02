1b44a6dc89183465f9653c73c79d667e307944a6
arifoglu <arifoglu2020@gmail.com>
2024-10-02 18:25:00 +0200

    labo7 done

--- sept_30_03/sept33/arifoglu_labo7.cpp
+++ sept_30_03/sept33/arifoglu_labo7.cpp
@@ -5,11 +5,13 @@
 */
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main(){
     // La moyen
+
     // La formul moyen => x = (1 / n) ∑'i=1 n' xi
 
     //la valeur initiale du nombre de nombre saisis  
@@ -24,14 +26,16 @@ int main(){
     //La somme des carrés des nombres
     double total_carre_des_numeros = 0.0;
 
-    //la variance
+    //la valeur initiale de la variance
     double variance = 0.0;
 
     cout << "Saisissez les valeurs pour calculer la moyenne. :\n";
     while(true){
        cin >> numeros;
+
        // une valeur negative ,la boucle se terminera
        if(numeros < 0){break;}
+
        /* 
        - À chaque fois va entre une valeur, elle sera ajoutée au total. 
        - Ensuite, le nombre d’entrées sera incrémenté d’une unité. 
@@ -45,13 +49,16 @@ int main(){
         -  la formul variance : v = (((∑'i=1 n' xi^2) - 1/n(∑'i=1 n' xi)^2)) / (n - 1);
         -  selon la formule =>  n == count;  
         */
+
         //  La somme des carrés des nombres saisis
         total_carre_des_numeros += pow(numeros,2); 
+
         // selon la formul variance
         variance = (total_carre_des_numeros - ((1.0 / count) * pow(total,2)))/ (count - 1.0);
 
     }
     cout << "La moyenne des valeurs saisies : " << moyen <<endl;
     cout << "La variance des valeurs saisies : " << variance <<endl;
+    
     return EXIT_SUCCESS;
 }
