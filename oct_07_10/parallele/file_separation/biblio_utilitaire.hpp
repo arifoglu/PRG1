3912ffb1de2675096e44e907d9bfaf2ca39cde64
arifoglu <arifoglu2020@gmail.com>
2024-10-10 11:48:15 +0200

    fonction separate have been seen

---
+++ oct_07_10/parallele/file_separation/biblio_utilitaire.hpp
@@ -0,0 +1,9 @@
+#ifndef BIBLIO_UTILITAIRE_HPP
+#define BIBLIO_UTILITAIRE_HPP
+
+void incremente_var_partagee();
+
+#endif
+
+// ce fichier pour => DECLARATION
+// seulement       => des fonctions prototypes
---
+++ oct_07_10/parallele/file_separation/main.cpp
@@ -0,0 +1,11 @@
+#include "biblio_principale.hpp"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main () {
+    imprime_valeur();
+    imprime_valeur();
+    return EXIT_SUCCESS;
+}
