62236ca638e6f1973b8a66ba8fce834c6e6b1df5
arifoglu <arifoglu2020@gmail.com>
2024-11-07 15:09:06 +0100

    labo16 done

--- nov_04_07/nov07/arifoglu_labo16.cpp
+++ nov_04_07/nov07/arifoglu_labo16.cpp
@@ -15,13 +15,9 @@ Buts: Manipulation avec des string, lecture dans un fichier, mesure du temps de 
 #include <cstdlib>
 #include <fstream>
 #include <chrono>
+#include "manipulations.hpp"
 using namespace std;
 using namespace std::chrono;
-
-string replace_all_GPT(string str,const string &from, const string & to );
-string replace_all(string str, const string & from, const string & to);
-string lire_fichier(const string & nom_fichier);
-string modifier_longeur(string str, int k);
 
 int main(int argc, char *argv[]){
    // Nous attendons 6 arguments depuis la ligne de commande
@@ -56,7 +52,7 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    //Sauvegarder le texte original
+    //Sauvegarder le texte original pour s'afficher a la fin du program
     string string_original = str;
 
     //Début du calcul du temps
@@ -81,14 +77,12 @@ int main(int argc, char *argv[]){
         str = replace_all(str,mot1,mot2);
     }
 
-
     //Fin du calcul du temps
     duration<double> time_span = duration_cast<duration<double>>(high_resolution_clock::now() - t1);
     //Affichage du temps
-    cout << "La nouvelle taille: " << str.length() << endl;
+    cout << "La nouvelle taille: " << str.length() << " bytes." << endl;
     cout << "La duréé : " << time_span.count() << " secondes ." << endl;
     
-
     //Restaurer le text original
     str = string_original;
 
@@ -96,88 +90,13 @@ int main(int argc, char *argv[]){
     cout << "Nous avons restauré le text original.\n"
          << "La taille du text original : "
          << str.length() 
+         << " bytes."
          << endl;
 
     return EXIT_SUCCESS;
 }
 
-string replace_all_GPT(string str,const string &from, const string & to ){
-    if(from.empty())
-    { 
-        // Si la partie à remplacer est vide, string elle-même est renvoyée  
-        return str;
-    }
-    // Initialise la position de départ à 0 pour commencer la recherche
-    size_t start_pos = 0;
-    // Effectuer le remplacement lors que 'from' est trouvée dans 'str'
-    while((start_pos = str.find(from,start_pos)) != std::string::npos )
-    {
-        //Remplace la sous-chaîne trouvée par 'to' à la position 'start_pos'
-        str.replace(start_pos,from.length(),to);
-        // Avance la position de départ de la longeur de 'to'
-        start_pos += to.length();
-    }
-    return str;
-} 
 
-string replace_all(string str, const string & from, const string & to){
-    //la position et la longueur du mot à changer
-    size_t position_from = str.find(from);
-    size_t longeur_from = from.length();
-
-    if(position_from == string::npos)
-    {
-        cout << "Le mot '" << from << "' n'a pas trouvé." << endl;
-    }
-    else
-    {
-        while(position_from != string::npos){
-              str.replace(position_from,longeur_from,to);
-              //Déterminer la nouvelle position après le changement du mot
-              position_from = str.find(from,position_from + to.length());
-        }                
-    }
-    return str;
-}
-
-string lire_fichier(const string & nom_fichier){
-    //Pour la lecture à partir d'un fichier
-    ifstream fichier(nom_fichier);
-    //Verifier si le fichier est ouvert
-    if(!fichier){
-        cerr << "Le fichier n'a pas pu ouvrir. ";
-        return ""; 
-    }
-    //Lire le fichier (line) et sauvegarde (contenu) en tant que string
-    string line, contenu; 
-    while(getline(fichier,line)){
-        contenu += line + "\n";
-    }
-    fichier.close();
-    return contenu;
-}
-
-string modifier_longeur(string str, int k){
-    //Définition de la limite pour la valeur de k
-    if(k < - 10 || k > 10){
-        cerr << "Erreur : k doit être entre (-10) et (20)." << endl;
-        return str;
-    }
-    // si k > 0 
-    if(k > 0)
-    {   //pour augmenter la longeur
-        for(int i = 0; i < k ;++i)
-        {
-            str += str;
-        }
-    }
-    // si k < 0
-    else if(k < 0)
-    {   //pour diminuer la longeur
-        str.resize(str.length() / (1 << - k));
-    }
-    return str;
-}
 
 
 
---
+++ nov_04_07/nov07/manipulations.hpp
@@ -0,0 +1,14 @@
+#ifndef MANIPULATIONS_HPP
+#define MANIPULATIONS_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string replace_all_GPT(string str,const string &from, const string & to );
+string replace_all(string str, const string & from, const string & to);
+string lire_fichier(const string & nom_fichier);
+string modifier_longeur(string str, int k);
+
+#endif 
