62ba69ce0258203b18126a5b0c87b40a49c87e01
arifoglu <arifoglu2020@gmail.com>
2024-12-11 16:05:24 +0100

    update

---
+++ exercices_c++/chapitre7/ex68/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+    private:
+        float x, y;
+        static int count;
+    public:
+        Point(float,float);
+        ~Point();
+        void deplace(float,float);
+        void affiche()const;
+};
+int Point::count = 0;
+
+Point::Point(float abs, float ord){
+    x = abs;
+    y = ord;
+    count++;
+}
+Point::~Point(){
+    count--;
+}
+
+void Point::deplace(float dx,float dy){
+    x += dx;
+    y += dy;
+}
+void Point::affiche()const{
+    cout << "objet "<< count << " : " ;
+    cout << "(" << x << "," << y << ")" <<endl;
+}
+
+int main () {
+    Point p1(1,6);
+    p1.affiche();
+    Point p2(3,8);
+    p2.affiche();
+    return 0;
+}
