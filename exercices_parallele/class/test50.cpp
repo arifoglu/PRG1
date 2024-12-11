5e8d52464be37212ce6b2f8ccc246a2bd8c769ab
arifoglu <arifoglu2020@gmail.com>
2024-12-11 08:50:16 +0100

    update

--- exercices_parallele/class/test50.cpp
+++ exercices_parallele/class/test50.cpp
@@ -14,6 +14,7 @@ class Point{
         Point operator+(Point rhs){
             rhs.x += x;
             rhs.y += y;
+            return rhs;
         }    
 
         void affiche()const {
@@ -22,11 +23,32 @@ class Point{
 };
 
 int main() {
-    Point p = 3.5;
-    p.affiche();
+    Point p = 3.5;          // conversion implicit double => int
+    p.affiche();            // (3,0)
 
-    Point q(5.5,6);
-    q.affiche();
+    Point q(5.5,6);         // conversion implicit double => int
+    q.affiche();            // (5,0)
+
+    Point r{6};             // 
+    r.affiche();            // (6,0)
+
+    int i = 6;
+
+    i = int(p);             //
+    cout << i <<endl;      // 3
+
+    i = 7 + p;
+    cout << i << endl;     // 10
+
+    p = 7 + q;             // Point(7 + int(q))
+    p.affiche();           // (10,0) 
+
+    p = Point(7) + q;      // Point(7,0) + (5.5,6)     
+    p.affiche();           // (12,6) 
+
+    q = p % q;             // Point(3,0) % (5.5,6) 
+    q.affiche();           // (0,0) 
+
 
     return EXIT_SUCCESS;
 }
