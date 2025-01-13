38a2b5c4c6e253a6b9c1902798febeeb564f1398
arifoglu <arifoglu2020@gmail.com>
2025-01-13 08:07:33 +0100

    update

--- exercices_parallele/revisions/revtest.cpp
+++ exercices_parallele/revisions/revtest.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
+#include <span>
 using namespace std;
 
-string lettres_majuscules_de(string const& str){
-    string resultat = "";
-    for(size_t e : str)
-        if(e >= 'A' && e <= 'Z')
-            resultat += e;
-    return resultat;
+size_t oter_hors_limite(span<double> s,const array<double,2>& arr){
+    size_t w = 0;
+    for(size_t i = 0; i < s.size();++i){
+        if(s[i] >= arr[0] && s[i] <= arr[1]){
+            s[w++] = s[i];
+        }
+    }
+    return w;
 }
 
 int main() { 
-   cout << lettres_majuscules_de("Hello, World !") << endl 
-        << lettres_majuscules_de("AbCDefgHIJklmnoP") << endl; 
+   array a{1.0, 2.3, 1.4, 5.3, 3.4, 2.4, 4.6}; 
+   auto n = oter_hors_limite(a, array{2.3, 5.0}); 
+   for (size_t i = 0; i < n; ++i) 
+      cout << a[i] << ' '; 
+ 
+   cout << endl; 
+   vector v{1.0, 2.3, 1.4, 5.3, 3.4, 2.4, 4.6}; 
+   v.resize(oter_hors_limite(v, array{1.4, 3.4})); 
+   for (double e : v) 
+      cout << e << ' '; 
 }
--- exercices_parallele/revisions/tests_finals/test11.cpp
+++ exercices_parallele/revisions/tests_finals/test11.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <span>
 using namespace std;
 
-int main() {
-    vector<int> v{2,3,5};
-    for(int val : v)
-        val *= val;
-    for(int i : v)
-        cout << i ;    
-    
-    return 0;
+using Mat = vector<vector<int>>;
+
+Mat generer(span<const int> val,span<const size_t> tailles){
+    Mat v(tailles.size());
+    size_t w = 0;
+    for(size_t i = 0; i<tailles.size();++i){
+        for(size_t j = 0; j < tailles[i];++j){
+            v[i].push_back(val[w++]);
+        }
+    }
+}
+
+int main() { 
+   { 
+      const vector<int> contenu{1, 2, 3, 4, 5, -1, -2, -3, -4, -5}; 
+      const array<size_t, 4> tailles{2, 3, 1, 4}; 
+      auto m = generer(contenu, tailles); 
+ 
+      for (auto const & v: m) { 
+         for (auto e: v) 
+            cout << setw(3) << e; 
+         cout << endl; 
+      } 
+   } 
+   cout << endl; 
+   { 
+      const array<int, 10> contenu{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+      const vector<size_t> tailles{5,3,2}; 
+      auto m = generer(contenu, tailles); 
+ 
+      for (auto const & v: m) { 
+         for (auto e: v) 
+            cout << setw(3) << e; 
+         cout << endl; 
+      } 
+   } 
 }
